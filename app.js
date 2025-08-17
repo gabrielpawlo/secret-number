9b5d49b0bc008105842338536d60e8ae61858804
Gabriel Pawlowski <gabrielgpawlowski@gmail.com>
2025-08-17 00:49:42 -0300

    SoundX

---
+++ app.js
@@ -0,0 +1,68 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = numeroAleatorio();
+let tentativas = 1;
+
+function exibirTexto(tag, texto){//parametro
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'UK English Male', {rate: 1.2});
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    numeroSecreto = numeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    mensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+function mensagemInicial(){
+    
+    exibirTexto('h1', 'Secret number game');
+    exibirTexto('p', 'Choose a number betwen 1 and 10');
+}
+
+function verificarChute(){//sem parametro
+    let chute = document.querySelector('input').value;
+
+    if(chute == numeroSecreto){
+        exibirTexto('h1', 'Correct!');
+        let palavraTentativas = tentativas > 1 ? 'tries' : 'try';
+        let mensagemTentativas = `Secret number discovered with ${tentativas} ${palavraTentativas}`
+        exibirTexto('p', mensagemTentativas);
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    } else {
+        if(chute > numeroSecreto){
+            exibirTexto('p', 'Secret number less than');
+        } else {
+            exibirTexto('p', 'Secret number more than');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function numeroAleatorio(){
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantiedadeDeElementos = listaDeNumerosSorteados.length;
+    if(quantiedadeDeElementos == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return numeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+}
+
+mensagemInicial();
